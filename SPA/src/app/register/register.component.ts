988781a7a70e3851847b596c6ea0d8e8cef79a10
VictorDomR <vdroyo@hotmail.com>
2023-11-10 20:46:14 -0600

    Seccion05 - Client registration and login

---
+++ SPA/src/app/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  // @Input() usersFromHomeComponent: any;
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit(): void {
+  }
+
+  register(): void {
+    this.accountService.register(this.model).subscribe({
+      next: () => {
+        this.finishRegistration();
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  finishRegistration(): void {
+    this.cancelRegister.emit(false);
+  }
+}
