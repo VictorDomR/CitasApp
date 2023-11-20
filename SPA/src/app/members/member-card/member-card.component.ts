76c80136960e3c14948f115ea00e90458a0eadad
VictorDomR <vdroyo@hotmail.com>
2023-11-19 20:47:13 -0600

    Seccion09 - Added member cards

---
+++ SPA/src/app/members/member-card/member-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IMember } from 'src/app/_models/imember';
+
+@Component({
+  selector: 'app-member-card',
+  templateUrl: './member-card.component.html',
+  styleUrls: ['./member-card.component.css']
+})
+export class MemberCardComponent implements OnInit {
+  // @Input() member: IMember = {} as IMember
+  // @Input() member: IMember | undefined;
+  @Input() member: IMember
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
